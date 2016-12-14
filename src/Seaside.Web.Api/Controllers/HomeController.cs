63399572e5db989c43f8814ece5ce56303457d50
jakescott <jake.net@gmail.com>
2016-12-14 15:56:29 +1300

    Add a basic .NET Core web app that we can use to test codedeploy

---
+++ src/Seaside.Web.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Seaside.Web.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Seaside!";
+        }
+    }
+}
