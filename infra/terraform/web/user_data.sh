48d1a4847c4907489db7aae0a109de64e456ce80
jakescott <jake.net@gmail.com>
2016-12-14 18:19:35 +1300

    Disable crap from dotnet cli

--- infra/terraform/web/user_data.sh
+++ infra/terraform/web/user_data.sh
@@ -21,3 +21,4 @@ sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotne
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
 sudo apt-get install dotnet-dev-1.0.0-preview2.1-003177 --assume-yes
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
